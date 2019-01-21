5342f9955b8f71b27b2f032273e0260224357494
NikiforovAll <nikiforovalekcey@gmail.com>
2019-01-21 14:46:53 +0200

    init commit

---
+++ ProjectScope.Client/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using XMindAPI.LIB;
+
+namespace ProjectScope.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileExtension = "xmind";
+            string defaultSheetName = "projectScope";
+            string dataSource = @"C:\Users\HYS\Downloads\internal_actionlist.xlsx";
+            XMindWorkBook book = new XMindWorkBook($"test.{fileExtension}");
+            ConfigureXMindWorkBook(book);
+            book.AddSheet(defaultSheetName);
+            book.Save();
+        }
+        public static void ConfigureXMindWorkBook(XMindWorkBook book)
+        {
+
+        }
+    }
+}
+    
